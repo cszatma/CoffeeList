990ffd740fd35f05e661a4066aad64f1bb5ed5c4
Christopher Szatmary <christopherszatmary@Christophers-iMac.local>
2017-02-08 19:32:47 -0500

    Commit Protocols

---
+++ CoffeeList/Protocols.swift
@@ -0,0 +1,22 @@
+//
+//  Protocols.swift
+//  CoffeeList
+//
+//  Created by Christopher Szatmary on 2016-06-21.
+//  Copyright © 2016 SzatmaryInc. All rights reserved.
+//
+
+import UIKit
+import CSKit
+
+protocol EntryViewer {
+    
+}
+
+extension EntryViewer {
+    
+}
+
+//extension DataSaver where Self: UIViewController {
+//    
+//}
