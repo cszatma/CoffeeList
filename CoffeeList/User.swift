6edbab3f287fe53042cddd53ac7936116e0b573c
cszatma <christopherszatmary@gmail.com>
2017-06-05 23:28:08 -0400

    Added User class to store and manage all data in one place.

---
+++ CoffeeList/User.swift
@@ -0,0 +1,55 @@
+//
+//  User.swift
+//  CoffeeList
+//
+//  Created by Christopher Szatmary on 2017-06-05.
+//  Copyright © 2017 SzatmaryInc. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    enum UserData {
+        case Entries
+        case EntryLists
+        case All
+    }
+    
+    static let instance = User()
+    var entries: Entries
+    var entryLists: EntryLists
+    
+    private init() {
+        entries = Entries.getFromUserDefaults(withKey: .SavedEntries) ?? Entries()
+        entryLists = EntryLists.getFromUserDefaults(withKey: .SavedLists) ?? EntryLists()
+    }
+    
+    func reload(selection: UserData) throws {
+        if selection == .Entries || selection == .All {
+            guard let savedEntries = Entries.getFromUserDefaults(withKey: .SavedEntries) else {
+                throw UserDefaultsError.NoEntriesSaved
+            }
+            entries = savedEntries
+        }
+        
+        if selection == .EntryLists || selection == .All {
+            guard let savedEntryLists = EntryLists.getFromUserDefaults(withKey: .SavedLists) else {
+                throw UserDefaultsError.NoEntryListsSaved
+            }
+            entryLists = savedEntryLists
+        }
+    }
+    
+    func save(selection: UserData) {
+        if selection == .Entries || selection == .All {
+            entries = sortEntryType(array: entries)
+            entries.saveToUserDefaults(withKey: .SavedEntries)
+        }
+        
+        if selection == .EntryLists || selection == .All {
+            entryLists = sortEntryType(array: entryLists)
+            entryLists.saveToUserDefaults(withKey: .SavedLists)
+        }
+    }
+}
