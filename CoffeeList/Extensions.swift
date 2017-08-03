b7cdda9662216a6e7fca3b79c32cf2fd1fc4f560
cszatma <christopherszatmary@gmail.com>
2017-08-03 12:52:32 -0400

    Added methods to UINavigationController for changing animations in push and pop.

--- CoffeeList/Extensions.swift
+++ CoffeeList/Extensions.swift
@@ -42,3 +42,41 @@ extension UITableViewCell {
 extension Array: UserDefaultsHandler {
     public typealias dataKey = UserDefaultsKeys
 }
+
+extension UINavigationController {
+    
+    func present(viewController: UIViewController, animationType: AnimationType?, duration: CFTimeInterval = 0.5) {
+        guard animationType != .push else {
+            pushViewController(viewController, animated: true)
+            return
+        }
+        
+        if let type = animationType {
+            view.layer.add(createTransition(type: type, duration: duration), forKey: nil)
+        }
+        
+        pushViewController(viewController, animated: false)
+    }
+    
+    @discardableResult
+    func popViewController(animationType: AnimationType?, duration: CFTimeInterval = 0.5) -> UIViewController? {
+        guard animationType != .push else {
+            return popViewController(animated: true)
+        }
+        
+        if let type = animationType {
+            view.layer.add(createTransition(type: type, duration: duration), forKey: nil)
+        }
+        
+        return popViewController(animated: false)
+    }
+    
+    private func createTransition(type: AnimationType, duration: CFTimeInterval) -> CATransition {
+        let transition = CATransition()
+        transition.duration = duration
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = type.rawValue
+        return transition
+    }
+    
+}
