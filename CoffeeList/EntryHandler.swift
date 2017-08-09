8b2571bfc4c9008503c1cb5205625df57bedb59b
cszatma <christopherszatmary@gmail.com>
2017-08-08 20:10:32 -0400

    Removed init(name:). Added notes property.

--- CoffeeList/EntryHandler.swift
+++ CoffeeList/EntryHandler.swift
@@ -12,10 +12,9 @@ import CSKit
 protocol EntryHandler: class, UserDefaultsHandler {
     
     var name: String { get set }
+    var notes: String { get set }
     var uid: UUID { get }
     typealias dataKey = UserDefaultsKeys
-    
-    init(name: String)
     
     ///Compares two EntryHandler instances of the same type. Comparision is based on all instance
     ///values being identical except for uid.
@@ -30,6 +29,9 @@ protocol EntryHandler: class, UserDefaultsHandler {
 
 extension EntryHandler {
     
+    /// Returns a Boolean value that indicates whether the receiver and a given entry type's properties are not equal.
+    /// - parameter to: The entry type to compare the receiver to.
+    /// - returns: `true` if the properties are not equal, `false` if they are.
     func isNotEqual(to: Self) -> Bool {
         return !(self.isEqual(to: to))
     }
