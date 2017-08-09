05b1868f0886575fca62154e1aa6b5ef120235a8
cszatma <christopherszatmary@gmail.com>
2017-08-08 20:09:34 -0400

    Replaced comments property with notes. It is no longer an optional as an empty string signifies no notes.

--- CoffeeList/Entry.swift
+++ CoffeeList/Entry.swift
@@ -18,44 +18,38 @@ class Entry : NSObject, NSCoding, EntryHandler {
     var name: String
     var coffeeType: String
     var favCoffeeShop: String
-    var comments: String?
+    var notes: String
     let uid: UUID
     
     ///Entry initialization
-    init(name: String, coffeeType: String, favCoffeeShop: String, comments: String?, uid: UUID = UUID()) {
+    init(name: String, coffeeType: String, favCoffeeShop: String, notes: String, uid: UUID = UUID()) {
         self.name = name
         self.coffeeType = coffeeType
         self.favCoffeeShop = favCoffeeShop
-        self.comments = comments
+        self.notes = notes
         self.uid = uid
-    }
-    
-    ///Required init from EntryHandler Protocol. Creates a default instance with the given name, unknown coffeeType and unknown favCoffeeShop.
-    /// - parameter name: Name of Entry.
-    required convenience init(name: String) {
-        self.init(name: name, coffeeType: "[unknown]", favCoffeeShop: "[unknown]", comments: nil)
     }
     
     required convenience init?(coder aDecoder: NSCoder) {
         let name = aDecoder.decodeObject(forKey: "name") as! String
         let coffeeType = aDecoder.decodeObject(forKey: "coffeeType") as! String
         let favCoffeeShop = aDecoder.decodeObject(forKey: "favCoffeeShop") as! String
-        let comments = aDecoder.decodeObject(forKey: "comments") as? String
+        let notes = aDecoder.decodeObject(forKey: "notes") as! String
         let uid = UUID(uuidString: aDecoder.decodeObject(forKey: "uid") as! String)!
-        self.init(name: name, coffeeType: coffeeType, favCoffeeShop: favCoffeeShop, comments: comments, uid: uid)
+        self.init(name: name, coffeeType: coffeeType, favCoffeeShop: favCoffeeShop, notes: notes, uid: uid)
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: "name")
         aCoder.encode(coffeeType, forKey: "coffeeType")
         aCoder.encode(favCoffeeShop, forKey: "favCoffeeShop")
-        aCoder.encode(comments, forKey: "comments")
+        aCoder.encode(notes, forKey: "notes")
         aCoder.encode(uid.uuidString, forKey: "uid")
     }
     
     ///String representation of Entry class. This allows type Entry to be converted into type String.
     override var description: String {
-        return "< \(name), \(coffeeType), \(favCoffeeShop), \(comments.hasValue), \(uid) >"
+        return "< \(name), \(coffeeType), \(favCoffeeShop), \(!notes.isEmpty), \(uid) >"
     }
     
     static func ==(lhs: Entry, rhs: Entry) -> Bool {
@@ -69,13 +63,13 @@ class Entry : NSObject, NSCoding, EntryHandler {
     ///Comparision of entries where uid doesn't matter.
     /// - parameter to: Entry istance is being compared to.
     func isEqual(to: Entry) -> Bool {
-        return (self.name == to.name) && (self.coffeeType == to.coffeeType) && (self.favCoffeeShop == to.favCoffeeShop) && (self.comments == to.comments)
+        return (self.name == to.name) && (self.coffeeType == to.coffeeType) && (self.favCoffeeShop == to.favCoffeeShop) && (self.notes == to.notes)
     }
     
-    func update(name: String, coffeeType: String, favCoffeeShop: String, comments: String?) {
+    func update(name: String, coffeeType: String, favCoffeeShop: String, notes: String) {
         self.name = name
         self.coffeeType = coffeeType
         self.favCoffeeShop = favCoffeeShop
-        self.comments = comments
+        self.notes = notes
     }
 }
