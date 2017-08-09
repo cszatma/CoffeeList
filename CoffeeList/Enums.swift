277f3b5dcff308fc120c6552226abb3cb48c179b
cszatma <christopherszatmary@gmail.com>
2017-08-08 20:07:18 -0400

    Removed SegueIdentifier.

--- CoffeeList/Enums.swift
+++ CoffeeList/Enums.swift
@@ -8,18 +8,6 @@
 
 import Foundation
 
-///List of Segue Identifiers for all segues relating to ViewControllers that deal with EntryHandler Types.
-enum EntryHandlerSegueIdentifiers: String {
-    ///Segue Identifier for showing EditEntryController.
-    case ShowEditEntry = "showEditEntry"
-    ///Segue Identifier for showing ViewEntryController.
-    case ShowViewEntry = "showViewEntry"
-    ///Segue Identifier for showing EditEntryListController.
-    case ShowEditEntryList = "showEditEntryList"
-    ///Segue Identifier for showing ViewEntryController.
-    case ShowViewEntryList = "showViewEntryList"
-}
-
 public enum UserDefaultsKeys: String {
     case SavedEntries = "savedEntries"
     case SavedLists = "savedLists"
