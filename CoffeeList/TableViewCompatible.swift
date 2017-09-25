5e667e27fc79e81f99be1a4db31e54fdf25bcf3b
cszatma <christopherszatmary@gmail.com>
2017-09-25 12:31:44 -0400

    Added protocol for types that can be displayed in a UITableView.

---
+++ CoffeeList/TableViewCompatible.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCompatible.swift
+//  CoffeeList
+//
+//  Created by Christopher Szatmary on 2017-08-11.
+//  Copyright © 2017 SzatmaryInc. All rights reserved.
+//
+
+import Foundation
