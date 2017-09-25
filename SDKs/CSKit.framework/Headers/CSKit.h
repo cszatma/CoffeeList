6359460c6e3c6e1b21ef5b7dd3a43fa64efa6ce8
cszatma <christopherszatmary@gmail.com>
2017-09-25 18:09:43 -0400

    Converted CSKit to Swift 4.

---
+++ SDKs/CSKit.framework/Headers/CSKit.h
@@ -0,0 +1,19 @@
+//
+//  CSKit.h
+//  CSKit
+//
+//  Created by Christopher Szatmary on 2017-09-25.
+//  Copyright © 2017 Christopher Szatmary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSKit.
+FOUNDATION_EXPORT double CSKitVersionNumber;
+
+//! Project version string for CSKit.
+FOUNDATION_EXPORT const unsigned char CSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSKit/PublicHeader.h>
+
+
